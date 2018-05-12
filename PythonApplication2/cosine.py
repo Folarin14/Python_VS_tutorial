23affda9ae62401e2e7401ebded9815303e220e6
Afolarin Lawal <pholalawal@yahoo.com>
2018-05-12 09:20:20 +0100

    random comments

--- PythonApplication2/cosine.py
+++ PythonApplication2/cosine.py
@@ -1,10 +1,10 @@
 from math import radians
 import numpy as np     # installed with matplotlib
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt #importing matplotlib
 
 def main():
-    x = np.arange(0, radians(1800), radians(12))
-    plt.plot(x, np.cos(x), 'b')
-    plt.show()
+    x = np.arange(0, radians(1800), radians(12)) # a range between 0 t0 1800rads at a 12 step interval
+    plt.plot(x, np.cos(x), 'b') # plots the result
+    plt.show() # displays the result
 
 main()
